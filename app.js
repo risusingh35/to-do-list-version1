14587d643f52cc6bcb74f389c4ed7692969ff716
Rishu Singh <risusingh18@gmail.com>
2021-09-12 15:01:46 +0530

    new commit with advance verilabe and function using to short code and exporting other js page and ejs

--- app.js
+++ app.js
@@ -1,45 +1,57 @@
 //jshint esversion:6
 const express =require("express");
 const bodyParser=require("body-parser");
+const date=require(__dirname+"/date.js")
 
 const app=express();
-
-//const livereload=require("livereload");
-
-//const connectLivereload=require("connect-livereload");
-
-//app.use(connectLivereload());
 
 
 app.use(express.static("public"));
  
 
-var items=["One-1","Two","Three","Four"];
+const items=["One","Two","Three","Four"];
+const workItem=[];
 
 app.set('view engine', 'ejs');
 
 app.use(bodyParser.urlencoded({extended:true}));
 
 app.get("/",function (req,res){
-
-    var today=new Date();
-
-    var options={
-        weekday:"long",
-        day:"numeric",
-        month:"long",
-        year:"numeric"
-    };
-    var day=today.toLocaleDateString("en-US",options);
-
-    res.render("list",{kindsOfDay:day,newListItems:items});
+    const day=date.getDate();
+  
+    res.render("list",{listTitle:day,newListItems:items});
 });
-app.post("/",function(req,res){
-  var item=req.body.newItem;
 
-    items.push(item);
-    res.redirect("/");
+app.post("/",function(req,res){  
+
+    const page=req.body.list;
+    
+    if (page==="work") {
+        
+        const item=req.body.newItem;
+        workItem.push(item);
+        res.redirect("/work");
+        
+    } else {
+       
+        const item=req.body.newItem;
+        items.push(item);
+        res.redirect("/"); 
+    }
+                
+    
+
+    
+});   
+
+
+app.get("/work", function (req,res){
+    res.render("list",{listTitle:"work", newListItems:workItem});
 });
+app.get("/about",function(res,res){
+    res.render("about");
+});
+
 app.listen(3000,function(){
     console.log("Server started on post 3000");
 });
